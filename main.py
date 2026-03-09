6937c3902b8e7ff252f640ee4bbd2928e1bb416b
sakosuri <sakosuri@cisco.com>
2026-03-09 21:59:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,29 +8,7 @@ Reads multiple CLI commands from an Excel file and executes them
 on all devices managed by Catalyst Center (DNA Center).
 
 Based on the original Cisco Sample Code by Gabriel Zapodeanu.
-
-Copyright (c) 2019 Cisco and/or its affiliates.
-
-This software is licensed to you under the terms of the Cisco Sample
-Code License, Version 1.1 (the "License"). You may obtain a copy of the
-License at
-
-               https://developer.cisco.com/docs/licenses
-
-All use of the material herein must be in accordance with the terms of
-the License. All rights not expressly granted by the License are
-reserved. Unless required by applicable law or agreed to separately in
-writing, software distributed under the License is distributed on an "AS
-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-or implied.
-
 """
-
-__author__ = "Gabriel Zapodeanu TME, ENB"
-__email__ = "gzapodea@cisco.com"
-__version__ = "0.2.0"
-__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
-__license__ = "Cisco Sample Code License, Version 1.1"
 
 import requests
 import json
