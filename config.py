5e7b0b82b5b1e3c1809549e89cdddf3dc2784c3f
sakosuri <sakosuri@cisco.com>
2026-03-09 22:00:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# This file contains the:
+# Cisco DNA Center username and password, server info
+
+# Update this section with the Cisco DNA Center server info and user information
+
+DNAC_URL = 'https://dna-center-ip-address'
+DNAC_USER = 'username'
+DNAC_PASS = 'password'
