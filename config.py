bce2fa862822aa099b6c6158ae0083e2368807d8
sakosuri <sakosuri@cisco.com>
2026-03-10 11:07:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # This file contains the:
 # Cisco DNA Center username and password, server info
 
-# Update this section with the Cisco DNA Center server info and user information
+# Update this section with the Cisco Catalyst Center (formerly known as DNA Center) server info and user information
 
-DNAC_URL = 'https://dna-center-ip-address'
+DNAC_URL = 'https://catalyst-center-ip-address'
 DNAC_USER = 'username'
 DNAC_PASS = 'password'
